e71534de2507c4d2e63b0d0be2463dbbfbcd355f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-25 18:33:28 +0000

    added function getExampleTasks in Saga

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone, fetchExampleTasks } from "../tasksSlice";
 import { ButtonsBox, Button } from "./styled";
 
 const Buttons = () => {
     const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
 
-    if (tasks.length === 0) {
-        return null;
-    }
+    
     return (
         <ButtonsBox>
-            <Button onClick={() => dispatch(toggleHideDone())}>
-                {hideDone ? "Pokaż" : "Ukryj"} ukończone
+            <Button onClick={() => dispatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
+            </Button>
+            {tasks.length !== 0 && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
         </Button>
-            <Button
-                onClick={() => dispatch(setAllDone())}
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
          </Button>
+                </>
+            )}
         </ButtonsBox>
     )
 };
--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -24,9 +24,13 @@ const tasksSlice = createSlice({
         setAllDone: state => {
             state.tasks.forEach(task => task.done = true);
         },
+        fetchExampleTasks: () => {},
+        setTasks: (state, {payload: tasks}) => {
+            state.tasks = tasks;
+        },
     },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, fetchExampleTasks, setTasks } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
